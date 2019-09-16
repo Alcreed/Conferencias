2c24a8922fae3d8ca1d2b1779ed8dd5c681d3a80
Herbert Alfonso Gonzalez Gutierrez <alfonso_gonzalezg@outlook.com>
2019-09-16 17:41:43 -0500

    Plugin jquery animateNumber
    Cuenta en aumento

--- js/main.js
+++ js/main.js
@@ -167,4 +167,10 @@ $(function(){
         return false;
     }
 
+    //Animaciones números
+    $('.resumen-evento span:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento span:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento span:nth-child(3) p').animateNumber({number: 3}, 1200);
+    $('.resumen-evento span:nth-child(4) p').animateNumber({number: 9}, 1200);
+
 });
